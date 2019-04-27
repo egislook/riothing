eb15771f67f1e545c79949299c08db5f4b2a3f78
Egis <lukas.egidijus@gmail.com>
2019-04-27 14:45:27 +0000

    working GA

--- server.js
+++ server.js
@@ -376,10 +376,8 @@ utils.getScript = (
           req.cookies = Cookies;
           req.hash = riothing.utils.qs(window.location.hash.replace('#', ''));
           req.query = riothing.utils.qs(req.querystring);
-          if(window.ga){
-            window.ga('set', 'page', window.location.href);
-            window.ga('send', 'pageview');
-          }
+          window.ga && window.ga('set', 'page', window.location.href);
+          window.ga && window.ga('send', 'pageview');
           next();
         });
         
